6aeba8397e2c3694144129da0fe4823d9f8b2c4a
Santiago Melo <santiagocano15cr7@gmail.com>
2023-11-22 19:30:06 -0600

    Carpeta 9, useReducer a fondo

---
+++ 09-api-usereducer/src/App.jsx
@@ -0,0 +1,36 @@
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
+import Product from './components/Product.jsx';
+import { CartContextProvider } from './store/Shopping-cart-context.jsx';
+
+
+const App = () => {
+
+  return (
+  
+    <CartContextProvider>
+    
+      <Header/>
+      
+      <Shop>
+
+        {DUMMY_PRODUCTS.map((product) => (
+    
+          <li key={product.id}>
+    
+            <Product {...product}/>
+    
+          </li>
+  
+        ))}
+
+      </Shop>
+    
+    </CartContextProvider>
+  
+  );
+
+}
+
+export default App;
---
+++ 09-api-usereducer/src/components/Cart.jsx
@@ -0,0 +1,87 @@
+import { useContext } from "react";
+import { CartContext } from "../store/Shopping-cart-context";
+
+export default function Cart() {
+
+  const {items, updatedItemQuantity} = useContext(CartContext)
+
+  const totalPrice = items.reduce(
+
+    (acc, item) => acc + item.price * item.quantity,
+
+    0
+
+  );
+
+  const formattedTotalPrice = `$${totalPrice.toFixed(2)}`;
+  
+  return (
+
+    // El consumer puede utilizarse para envolver el codigo que deba tener acceso a un valor de contexto
+
+    //<CartContext.Consumer></CartContext.Consumer>
+
+          
+    <div id="cart">
+    
+      {items.length === 0 && <p>No items in cart!</p>}
+    
+      {items.length > 0 && (
+    
+        <ul id="cart-items">
+        
+          {items.map((item) => {
+        
+            const formattedPrice = `$${item.price.toFixed(2)}`;
+
+            return (
+            
+              <li key={item.id}>
+              
+                <div>
+              
+                  <span>{item.name}</span>
+              
+                  <span> ({formattedPrice})</span>
+              
+                </div>
+              
+                <div className="cart-item-actions">
+              
+                  <button onClick={() => updatedItemQuantity(item.id, -1)}>
+              
+                    -
+             
+                  </button>
+             
+                  <span>{item.quantity}</span>
+             
+                  <button onClick={() => updatedItemQuantity(item.id, 1)}>
+             
+                    +
+             
+                  </button>
+             
+                </div>
+             
+              </li>
+            
+            );
+          
+          })}
+        
+        </ul>
+      
+      )}
+      
+      <p id="cart-total-price">
+      
+        Cart Total: <strong>{formattedTotalPrice}</strong>
+      
+      </p>
+    
+    </div>
+          
+  )
+      
+}

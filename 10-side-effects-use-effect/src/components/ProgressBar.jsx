91b10aad8a545db0a951e165027904eece30837a
Santiago Melo <santiagocano15cr7@gmail.com>
2023-11-22 19:33:02 -0600

    Carpeta 10, useEffect a fondo

---
+++ 10-side-effects-use-effect/src/components/ProgressBar.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react"
+
+
+export const ProgressBar = ({timer}) => {
+    
+    const [remainingTime, setRemainingTime] = useState(timer)
+
+    useEffect(() => {
+      
+      const interval = setInterval(() => { 
+      
+        setRemainingTime((prevTime) => prevTime - 10)
+      
+      }, 10)
+  
+      return () => {
+      
+        clearInterval(interval)
+      
+      }
+  
+    }, [])
+
+    return <progress value={remainingTime} max={timer}></progress>
+
+}

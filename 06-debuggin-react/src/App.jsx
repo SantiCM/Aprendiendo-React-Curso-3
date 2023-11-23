52b6101d6be6c19c7f8940af31d921dce76bc153
Santiago Melo <santiagocano15cr7@gmail.com>
2023-11-22 21:03:02 -0600

    Carpeta 6, Debbugin

--- 06-debuggin-react/src/App.jsx
+++ 06-debuggin-react/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header.jsx';
 import UserInput from './components/UserInput.jsx';
 import Results from './components/Results.jsx';
 
+// app de investement
 function App() {
 
   const [userInput, setUserInput] = useState({
